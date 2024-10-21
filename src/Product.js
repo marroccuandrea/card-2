9c8709f463c3539e6067cd7a66d3279b3bb3d485
marroccuandrea <marroccuandrea@yahoo.it>
2024-10-21 18:27:05 +0200

    [add] onClick pulsante

--- src/Product.js
+++ src/Product.js
@@ -4,18 +4,36 @@ import React from 'react'
 // const Product = ({prezzoFinale, nome, img, prezzo, utente})
 const Product = ({prezzoFinale, nome, img, prezzo, utente}) => {
 
+  //Aggiungo una reattività al bottone con onClick
+  const handleClick = () => {
+    console.log("Mi hai premuto");
+  };
+
+  const paramsHandler = (prezzo) => {
+    console.log(prezzo);
+    
+  };
+
+  function prezzoPieno(prezzo) {
+    return console.log(prezzo);
+    
+  }
 
   return (
     <article>
         <div className="card">
-            <img src={img} alt={`${nome}`} />
+            <img src={img} alt={`${nome}`} onClick={handleClick}/> {/* Al click sulla foto */}
             <h6>{nome}</h6>
             <hr />
             <p>{prezzo} €</p>
             <p>{nome}</p>
             <p style={{textTransform: 'uppercase'}}>{utente}</p>
             <p className="card-time">RIAPRE PRESTO</p>
-          <button>{prezzoFinale} €</button>
+          <button onClick={()=> alert("Bottone premuto")}>{prezzoFinale} €</button>
+          <button onClick={()=> alert(`${nome}`)}>{prezzoFinale} €</button>
+          <button onClick={()=> paramsHandler(prezzo)}>{prezzoFinale} €</button>
+          <button onClick={()=> prezzoPieno(prezzo)}>{prezzoFinale} €</button>
+          {/* Se si richiama una funzione è essenziale l'arrow function ()=> altrimenti esse partiranno all'avvio della applicazione*/}
         </div>
         
     </article>
